d4e0990577c5a45688cf1caec4060694c3f7333b
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-08 23:24:57 +0300

    tie everything in main

--- todo_app.rb
+++ todo_app.rb
@@ -1,3 +1,26 @@
+# todo_app.rb
+
 require_relative 'task_manager'
 
-tasks = []
+tasks = load_tasks
+
+loop do
+  display_menu
+  choice = get_user_choice
+
+  case choice
+  when 1
+    add_task(tasks)
+  when 2
+    view_tasks(tasks)
+  when 3
+    mark_task_as_complete(tasks)
+  when 4
+    remove_task(tasks)
+  when 5
+    puts "Goodbye!"
+    break # Exit the loop
+  else
+    puts "Invalid choice. Please enter a valid option (1-5)."
+  end
+end
