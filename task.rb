0ddf8634c032bff904310c013ff6e8aa830197e4
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-08 23:48:25 +0300

    edit task module

--- task.rb
+++ task.rb
@@ -1,9 +1,9 @@
 class Task
     attr_accessor :title, :description, :completed
 
-    def initialize(title, description, completed)
+    def initialize(title, description, completed = false)
         @title = title
         @description = description
-        @completed = false
+        @completed = completed
     end
 end
