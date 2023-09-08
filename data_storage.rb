c844202eedc1b511423956ebf746050fba0c9603
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-08 23:18:46 +0300

    edit data storage model

--- data_storage.rb
+++ data_storage.rb
@@ -1,21 +1,21 @@
+# data_storage.rb
+
 require_relative 'task'
 require_relative 'constants'
 
 def load_tasks
-    tasks = []
-
-    if File.exist?(TASKS_FILE_PATH)
-        File.open(TASKS_FILE_PATH, 'r') do |file|
-            file.each_line do |line|
-                title, description, completed = line.chomp.split(',')
-                tasks = Task.new(title, description)
-                task.completed = (completed == 'true')
-                tasks.push(task)
-            end
-        end
+  tasks = []
+  if File.exist?(TASKS_FILE_PATH)
+    File.open(TASKS_FILE_PATH, 'r') do |file|
+      file.each_line do |line|
+        title, description, completed = line.chomp.split(',')
+        task = Task.new(title, description)
+        task.completed = (completed == 'true')
+        tasks.push(task)
+      end
     end
-    tasks
-
+  end
+  tasks
 end
 
 def save_tasks(tasks)
